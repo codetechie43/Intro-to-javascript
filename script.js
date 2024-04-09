900b12502375a75c5ce71a184ebc537552649c46
codetechie43 <sujalkhade676@gmail.com>
2024-04-09 23:25:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Rule of createing a varible 
+ 1. the name should be unique.
+ 2. It should not be a keyword.
+ 3. It should not start with a number.
+ 4. It should not start with an identifier.
+ 5. it can start with only $ and _ */
+
+// We created a new variable using the const variable
+const _sujuBhai = "Hello suju, How are you?";
+
+// here we cretaed a new variable using the let variable
+let $ajjuBhai = "How are you?";
+
+// here we created a new variable using the var variable
+var sunitaDevi = "How are you ajjubhai?";
+
+// here we displayed output using console.log
+console.log(_sujuBhai);
+
+//here we displayed output using console.log
+console.log(sunitaDevi); 
+
+// here we displayed output using console.log
+console.log($ajjuBhai); // console
+
+
+// Starting data type "string"
+ const namexampleString = "Sunita Devi";
+ const singleQuote='tu hai pani pani';
+ console.log(namexampleString);
+ console.log(singleQuote);
