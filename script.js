03ce0aa168aaf8be52376ad616dba1a217763af5
codetechie43 <sujalkhade676@gmail.com>
2024-04-12 01:44:18 +0530

    Data types and Object

--- script.js
+++ script.js
@@ -96,7 +96,83 @@ const backTicks = `hello ${doubleQuote}`;
  console.log(date);
 
 
- 
+ // Statically types language
+ let message ='hello suju'
+ console.log(typeof message);
+
+ message =true;
+ console.log(typeof  message);
+ // dynamically types language 
+ // Javascript is dynamically typed language
+
+
+ // Comaparison operation ==> true/false
+
+ const a = 5;
+ const b = 18;
+
+ //greater tham 
+ console.log(a > b);
+
+ //less than
+ console.log(a < b);
+
+ //equal to
+ console.log(a == b);
+
+ //not equal to
+ console.log(a!= b);
+
+ //greater than or equal to
+ console.log(a >= b);
+
+ //less than or equal to
+ console.log(a >= b); 
+
+ //strict equality
+ console.log(a === b);
+
+ //strict inequality :compares values and data types
+ //return true when both the values are true
+ console.log(a!== b);
+
+ console.log(7===7);
+ console.log("suju"==="suju");
+
+ // loose equality
+ console.log(5 == "5");
+
+ // loose inequality
+ console.log(a!== "suju");
+
+ console.log(7===7);
+ console.log("suju"==="suju");
+
+ // loose equality
+ // the good ones :- ==== !== 
+ // the bad ones :- ==  !=
+
+ console.log(0 ==='0');
+ console.log(true ==='1');
+
+
+ // logical operators
+ // OR ==> either of the conditions should be true
+console.log(true || false);
+
+ //AND && ==> both the conditions should be true
+ console.log(true && false);
+
+ //NOT ==> !
+ console.log(!false);
+
+
+
+
+
+
+
+
  
 
  
